7d81a12530ada8ed170d1f6ee35277824f343b67
ashirwad5555 <ashirwadkatkamwar@gmail.com>
2024-08-12 00:54:12 +0530

    imgage error solved

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import img from "../assets/mavs_main_logoHD.png";
 
 function Logo({ width = "100px" }) {
   return (
     <div className="text-2xl font-bold text-slate-900">
-      <img src="src\assets\mavs_main_logoHD.png" alt="MAVS" width={width} />
+      <img src={ img } alt="MAVS" width={width} />
     </div>
   );
 }
