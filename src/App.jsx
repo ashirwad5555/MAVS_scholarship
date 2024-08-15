76f5d9f04da4f8f7a8f3634b5bebb66542d75661
ashirwad5555 <ashirwadkatkamwar@gmail.com>
2024-08-15 12:14:02 +0530

    25 min logout Timer applyed

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import authService from "./appwrite/auth";
 import authSlice, { login, logout } from "./store/authSlice";
 import { Footer, Header } from "./components";
 import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [inactiveTime, setInactiveTime] = useState(0); //added on demand
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     authService
@@ -22,6 +25,47 @@ function App() {
       })
       .finally(() => setLoading(false));
   }, []);
+
+  //added on demand
+  useEffect(() => {
+    const events = ["click", "mousemove", "keypress"];
+
+    const resetTimer = () => {
+      setInactiveTime(0);
+    };
+
+    //my logout logic
+    const logoutHandler = () => {
+      authService.logout().then(() => {
+        dispatch(logout());
+        navigate("/"); // Navigate to the homepage //added while testing
+      });
+    };
+
+    const increaseTimer = () => {
+      setInactiveTime((prevTime) => {
+        if (prevTime >= 1800) {
+          // 25 minutes
+          logoutHandler(); // Call the logoutHandler when the timer reaches the threshold
+          clearInterval(timer);
+        }
+        return prevTime + 1;
+      });
+    };
+
+    events.forEach((event) => {
+      window.addEventListener(event, resetTimer);
+    });
+
+    const timer = setInterval(increaseTimer, 1000); // Increases timer every second
+
+    return () => {
+      events.forEach((event) => {
+        window.removeEventListener(event, resetTimer);
+      });
+      clearInterval(timer);
+    };
+  }, [navigate]);
 
   return !loading ? (
     <div className="min-h-screen w-full flex flex-wrap content-between bg-green-100 rounded-xl">
